5ed3ebf25c9bf53522bcc0e2ece14d277cbd2041
falecomluanbispo <falecomluanbispo@gmail.com>
2018-08-18 12:14:49 -0300

    EDIT 1 Dashboard do Modulo Tr
    Este commit consiste as primeiras informações do cabeçalho da página principal do módulo de treinamento.

--- resources/views/Treinamento/index.blade.php
+++ resources/views/Treinamento/index.blade.php
@@ -7,5 +7,56 @@
 @stop
 
 @section('content')
-    Quisque et tellus consectetur, molestie ante eget, maximus lorem. Duis varius est sed ipsum pellentesque tincidunt. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nunc nec ante mi. Nullam vestibulum quis augue id elementum. Aenean ac nisl a odio porta tincidunt et et mauris. Maecenas eget fringilla ante, at feugiat lacus. Aliquam aliquam at tellus sit amet sodales. Aliquam sed tellus sed justo pulvinar tincidunt. Praesent pellentesque libero nisl, sed aliquet elit tristique aliquam. Nunc interdum nisi aliquam est accumsan, vel sagittis nulla placerat. Suspendisse potenti.
+    <div class="row">
+        <!-- ./col -->
+        <div class="col-lg-4 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-green">
+            <div class="inner">
+              <h3>53<sup style="font-size: 10px">Coloque % aqui</sup></h3>
+
+              <p>Treinamentos Concluidos</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-stats-bars"></i>
+            </div>
+            <a href="#" class="small-box-footer">Veja mais... <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+        <!-- ./col -->
+        <div class="col-lg-4 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-yellow">
+            <div class="inner">
+              <h3>44</h3>
+              <p>Treinamentos Pendentes</p>
+            </div>
+            <div class="icon">
+              <i class="fa fa-calendar"></i>
+            </div>
+            <a href="#" class="small-box-footer">Veja mais... <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+        <!-- ./col -->
+        <div class="col-lg-4 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-blue">
+            <div class="inner">
+              <h3>65</h3>
+
+              <p>Treinamentos Programados</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-pie-graph"></i>
+            </div>
+            <a href="#" class="small-box-footer">Veja mais... <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+        <!-- ./col -->
+      </div>
+
+      <div class="box box-success" style="position: relative; left: 0px; top: 0px;">
+       <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+              
+       </div>
 @stop
